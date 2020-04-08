303654acc125b1bbdc9a06a5a654a8b76c84835a
kerlomz <kerlomz@gmail.com>
2020-04-08 18:06:03 +0800

    FEATURE: 认证码生成器

--- nv_client/nv/utils.py
+++ nv_client/nv/utils.py
@@ -2,12 +2,16 @@
 # -*- coding:utf-8 -*-
 # Author: kerlomz <kerlomz@gmail.com>
 import os
+import win32api
+import win32con
 from nv.config import LICENSE_PATH
 
 
 def set_license(content):
+    win32api.SetFileAttributes(LICENSE_PATH, win32con.FILE_ATTRIBUTE_NORMAL)
     with open(LICENSE_PATH, 'w', encoding="utf-8") as fp:
         fp.write(content)
+    win32api.SetFileAttributes(LICENSE_PATH, win32con.FILE_ATTRIBUTE_HIDDEN + win32con.FILE_ATTRIBUTE_SYSTEM)
 
 
 def read_license():
